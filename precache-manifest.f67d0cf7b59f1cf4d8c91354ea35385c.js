ce3831f92398744281eb53298f73a91ebeb2ad49
arnavigode <arnavi.gode96@gmail.com>
2021-11-28 00:59:30 +0530

    Updates

---
+++ precache-manifest.f67d0cf7b59f1cf4d8c91354ea35385c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08ab58aabb72d563ab612158099d9ecb",
+    "url": "/my-Portfolio-/index.html"
+  },
+  {
+    "revision": "0adb2e1f9113c642d8bd",
+    "url": "/my-Portfolio-/static/css/2.e45429e2.chunk.css"
+  },
+  {
+    "revision": "23de42ae43e87819d2b2",
+    "url": "/my-Portfolio-/static/css/main.f0c39ea1.chunk.css"
+  },
+  {
+    "revision": "0adb2e1f9113c642d8bd",
+    "url": "/my-Portfolio-/static/js/2.ab1b0775.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/my-Portfolio-/static/js/2.ab1b0775.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23de42ae43e87819d2b2",
+    "url": "/my-Portfolio-/static/js/main.bbf21171.chunk.js"
+  },
+  {
+    "revision": "7d81b8b6b5e8a2deab37",
+    "url": "/my-Portfolio-/static/js/runtime-main.4cdcb41b.js"
+  }
+]);
